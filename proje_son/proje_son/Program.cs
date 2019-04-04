85b5f7ace4d24efcb263b9cff4dc631f30af9ce1
simgey <2016280032@ogr.deu.edu.tr>
2019-04-04 19:36:16 +0300

    asdhga

--- proje_son/proje_son/Program.cs
+++ proje_son/proje_son/Program.cs
@@ -6,7 +6,7 @@ namespace proje_son
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("selam");
         }
     }
 }
